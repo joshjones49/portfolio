916ed3a8c4c030e461e486cdf259e0fee541c359
joshjones49 <joshjones200285@gmail.com>
2025-08-02 14:57:05 -0400

    Starting portfolio over

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css'
 
 // component imports
-import Taskbar from './components/Taskbar/Taskbar.jsx'
-import Hero from './components/Hero/Hero.jsx'
+
 
 function App() {
 
   return (
     <>
-      <Hero />
-      <Taskbar />
+
     </>
   )
 }
